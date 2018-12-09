1d47aed57ebcbdb3f0630f41b4b0812acbed25f0
songpang <scc6920@naver.com>
2018-12-09 22:34:01 +0900

    Starcount detail , footprint modify

--- public/assets/js/myrecipe.js
+++ public/assets/js/myrecipe.js
@@ -10,43 +10,12 @@ function myrecipeview() {
 
             targeturl = targeturl
             $("#rank1").attr("href", targeturl)
-            $("#rank1").text("Song")
+            $("#rank1").text("Lastest Recipe Link Here")
             // window.location.href = 'message.likepost';
             console.log(targeturl);
         });
     }
 }
-
-
-function param() {
-
-    var url = "http://127.0.0.1:7001/abc/LoginForm.action?userId=para&userNm=파람";
-    //var url = document.location.href;
-
-    if (url.indexOf("?") > -1) {
-        var splits = url.split("?"); //split("구분자"):tokenizer와 다른점은 split는 공백도 하나의 값을 가진다.
-        var gets = splits[1];
-
-        var para = gets.split("&");
-        var len = para.length;
-
-        for (var i = 0; i < len; i++) {
-            var param = para[i].split("=");
-            var name = param[0];
-            var value = param[1];
-
-            alert("name : " + name);
-            alert("value : " + value);
-
-        }
-
-    }
-
-}
-
-
-
-
 
 function likepostup() {
     var user = firebase.auth().currentUser;
